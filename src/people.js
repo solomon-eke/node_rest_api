cd7dde58d7cd9982a152f119c61f5a8cfe196a81
Solomon <ekesolomon11@gmail.com>
2024-12-09 11:15:38 -0500

    final part

---
+++ src/people.js
@@ -0,0 +1,18 @@
+
+
+export let people = [{
+    name: "John",
+    age: 35,
+    hairColor: "brown"
+},
+{
+    name: "Joe",
+    age: 20,
+    hairColor: "black"
+
+},
+{
+    name: "Jane",
+    age: 40,
+    hairColor: "blonde"
+}]
